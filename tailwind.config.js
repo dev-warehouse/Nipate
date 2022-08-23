666818dff20c3d780ff11ea6ede834718f152330
Dev Banda <wbanda@kabarak.ac.ke>
2022-08-23 12:07:46 +0300

    ☕ Added custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "brand": "#DF0000",
+        "brand_yellow": "#FFDD00",
+        "brand_gold": "#FF9900",
+        "input_bg": "#F1F5F7"
+      }
+    },
   },
   plugins: [],
 }
