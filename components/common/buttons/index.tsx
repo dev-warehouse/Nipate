712c996c5a25e72efa266df0f1f31b6a3b3c0223
Dev Banda <wbanda@kabarak.ac.ke>
2022-08-23 12:30:49 +0300

    📖 Finilizing Button Component with Documentation

--- components/common/buttons/index.tsx
+++ components/common/buttons/index.tsx
@@ -1,10 +1,29 @@
 import {ButtonHTMLAttributes} from "react";
 import styles from './index.module.scss'
 
+/**
+ * Adds on Html Button Attributes with variant to create custom button element
+ */
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    /**
+     * ## Variant of Button, has three variants,
+     *
+     * `text` - renders text button
+     *
+     * `outline` - renders button with outline
+     *
+     * `solid` - renders the default filled button
+     */
     variant?: 'text' | 'outline' | 'solid'
 }
 
+/**
+ * Themed button, extends html button and adds on variants.
+ * @param variant
+ * @param className
+ * @param props
+ * @constructor
+ */
 const Button = ({variant = 'solid', className, ...props}: ButtonProps) => {
     return <button
         className={[className, styles.base, variant === 'text' ? styles.text : variant == 'outline' ? styles.outline : styles.solid].join(' ')} {...props}/>
