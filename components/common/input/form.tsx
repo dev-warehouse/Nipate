3940f43b0bdba7970e86653e6104fe9619f65155
Dev Banda <wbanda@kabarak.ac.ke>
2022-08-24 11:40:09 +0300

    ☕ Added Form Input Validation

--- components/common/input/form.tsx
+++ components/common/input/form.tsx
@@ -1,14 +1,39 @@
 import {Input, InputProps} from ".";
 import styles from './index.module.scss'
+import {ReactNode} from "react";
 
+/**
+ * Form Input props, extends Input Props
+ *
+ * adds upon inputLabel for form label
+ */
 interface FormInputProps extends InputProps {
+    /**
+     * Form Input Label
+     */
     inputLabel: string
+    /**
+     * Feedback message
+     */
+    feedback?: string
+    /**
+     * Specifying children overrides default Input Component
+     */
+    children?: ReactNode
 }
 
-const FormInput = (props: FormInputProps) => {
+/**
+ * Input for Forms with label and validation
+ * @param inputLabel
+ * @param props
+ * @constructor
+ */
+const FormInput = ({inputLabel, ...props}: FormInputProps) => {
     return <div className={styles.form_root}>
-        <label className={styles.form_label}>{props.inputLabel}</label>
-        <Input className={styles.form_input} {...props}/>
+        <label className={styles.form_label} data-validity={props.dataValidity}>{inputLabel}</label>
+        {props.children ?? <Input className={styles.form_input} {...props}/>}
+        {props.feedback !== undefined ? <p className={styles.form_message} data-validity={props.dataValidity}>{props.feedback}</p> : <></>}
     </div>
 }
+
 export {FormInput}
