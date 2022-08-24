cbd1699f659f845913dab0db7f98f6c1b5bc1de4
Dev Banda <wbanda@kabarak.ac.ke>
2022-08-24 10:31:09 +0300

    ☕ Added Form Input Props

--- components/common/input/form.tsx
+++ components/common/input/form.tsx
@@ -2,12 +2,12 @@ import {Input, InputProps} from ".";
 import styles from './index.module.scss'
 
 interface FormInputProps extends InputProps {
-
+    inputLabel: string
 }
 
 const FormInput = (props: FormInputProps) => {
     return <div className={styles.form_root}>
-        <label className={styles.form_label}>Label</label>
+        <label className={styles.form_label}>{props.inputLabel}</label>
         <Input className={styles.form_input} {...props}/>
     </div>
 }
