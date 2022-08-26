3eff6b919c7ed21d5ba1466b8b0e1c2a482a8f59
Dev Banda <wbanda@kabarak.ac.ke>
2022-08-27 00:05:47 +0300

    ☕ Added Auth route page markup

---
+++ pages/auth/index.tsx
@@ -0,0 +1,10 @@
+import {NextPageWithLayout} from "@pages/_app";
+import {AuthLayout} from "@components/layouts";
+
+const Index: NextPageWithLayout = () => {
+    return <div>
+        Auth Page
+    </div>
+}
+
+export default Index
