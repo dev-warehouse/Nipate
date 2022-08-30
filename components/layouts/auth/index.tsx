4b87525bea5f03744f50287cd66a79973f1aa4b7
Dev Banda <wbanda@kabarak.ac.ke>
2022-08-30 21:28:21 +0300

    ☕ Changed Auth Page variant to Auth

--- components/layouts/auth/index.tsx
+++ components/layouts/auth/index.tsx
@@ -3,7 +3,7 @@ import {Header} from "@components/pages";
 
 export function AuthLayout(props: HTMLAttributes<HTMLDivElement>) {
     return <div>
-        <Header/>
+        <Header variant={'auth'}/>
         <main>{props.children}</main>
     </div>
 }
