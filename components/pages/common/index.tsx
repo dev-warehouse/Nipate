10b00485c202fbc7b3b09c7fb5f53d357295bf38
Dev Banda <wbanda@kabarak.ac.ke>
2022-08-30 21:29:41 +0300

    ☕ Changed Header Component to Class Components

--- components/pages/common/index.tsx
+++ components/pages/common/index.tsx
@@ -1,11 +1,16 @@
-import {HTMLAttributes} from "react";
+import {Component, HTMLAttributes} from "react";
 import Image from "next/image";
 import Logo from '/public/assets/logo_full.svg'
 import {Button} from "@components/common";
 
-const Header = (props: HTMLAttributes<HTMLDivElement>) => {
-    return <div className={"flex flex-row items-center justify-between"}>
-        <div style={{height: "1.8rem", width: "8rem", position: "relative"}}>
+interface HeaderProps extends HTMLAttributes<HTMLDivElement> {
+    variant?: 'auth' | 'page'
+}
+
+class Header extends Component<HeaderProps> {
+
+    Logo = (): JSX.Element => {
+        return <div style={{height: "1.8rem", width: "8rem", position: "relative"}}>
             <Image src={Logo} alt={"App Logo"} layout={'fill'}/>
         </div>
         <div className={"flex flex-row gap-1"}>
