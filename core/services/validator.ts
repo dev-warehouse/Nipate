bc03db38b38ed36723014931cf80c95592fefea4
Dev Banda <wbanda@kabarak.ac.ke>
2022-09-03 09:21:09 +0300

    ☕ Added Validator service methods

--- core/services/validator.ts
+++ core/services/validator.ts
@@ -1,1 +1,51 @@
-export {}
+import {LoginFormData, RegisterFormData, UserModel} from "@core/models";
+
+/**
+ * This class deals with all containing validation
+ *
+ * It encompasses methods that do validation of text, form, objects etc
+ */
+class Validator {
+    /**
+     * This Validates one identification number, checks if it's in its correct format
+     * @param id
+     */
+    static validateIdentification(id: UserModel["idNumber"]): boolean {
+        return false;
+    }
+
+    /**
+     * This validates ones mobile number according to the specified country code
+     * @param mobile
+     */
+    static validateMobileNumber(mobile: UserModel["mobileNumber"]): boolean {
+        return false;
+    }
+
+    /**
+     * This validates the password details given, for right now its basic with it
+     * validating that the password should be a word and not less than 8 digits
+     * @param password
+     */
+    static validatePassword(password: LoginFormData["password"]): boolean {
+        return false;
+    }
+
+    /**
+     * This validates the whole Login form data
+     * @param model
+     */
+    static validateLoginDetails(model: LoginFormData): boolean {
+        return false;
+    }
+
+    /**
+     * This validates the whole register form data
+     * @param model
+     */
+    static validateRegisterDetails(model: RegisterFormData): boolean {
+        return false
+    }
+}
+
+export {Validator}
