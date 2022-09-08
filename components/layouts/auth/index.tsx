ae8928d4a903a3009be1f413a3f3827501c679dc
Dev Banda <wbanda@kabarak.ac.ke>
2022-09-08 19:21:23 +0300

    ☕ Added styles to Auth layout

--- components/layouts/auth/index.tsx
+++ components/layouts/auth/index.tsx
@@ -1,8 +1,9 @@
 import {HTMLAttributes} from "react";
 import {Header} from "@components/pages";
+import styles from './index.module.scss'
 
 export function AuthLayout(props: HTMLAttributes<HTMLDivElement>) {
-    return <div>
+    return <div className={styles.auth_root}>
         <Header page={'auth'}/>
         <main>{props.children}</main>
     </div>
