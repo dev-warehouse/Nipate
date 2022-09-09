6f746750e0a8229e9debd506938367a0d1bc8b85
Dev Banda <wbanda@kabarak.ac.ke>
2022-09-09 11:10:47 +0300

    ☕ Added Footer and move styles to styles file

--- components/layouts/auth/index.tsx
+++ components/layouts/auth/index.tsx
@@ -1,10 +1,11 @@
 import {HTMLAttributes} from "react";
-import {Header} from "@components/pages";
+import {Footer, Header} from "@components/pages";
 import styles from './index.module.scss'
 
 export function AuthLayout(props: HTMLAttributes<HTMLDivElement>) {
     return <div className={styles.auth_root}>
         <Header page={'auth'}/>
-        <main>{props.children}</main>
+        <main className={styles.auth_main}>{props.children}</main>
+        <Footer/>
     </div>
 }
