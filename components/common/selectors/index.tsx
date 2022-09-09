6fcef6933639f3a8134c706142af0775681cba0d
Dev Banda <wbanda@kabarak.ac.ke>
2022-09-09 12:10:34 +0300

    ☕ Added Checkbox & Radio components

--- components/common/selectors/index.tsx
+++ components/common/selectors/index.tsx
@@ -1,1 +1,12 @@
-export {}
+import {InputHTMLAttributes} from "react";
+import styles from './index.module.scss'
+
+function CheckBox(props: InputHTMLAttributes<HTMLInputElement>) {
+    return <input type="checkbox" className={styles.checkbox} {...props}/>
+}
+
+function Radio(props: InputHTMLAttributes<HTMLInputElement>) {
+    return <input type="radio" className={styles.radio} {...props}/>
+}
+
+export {CheckBox, Radio}
