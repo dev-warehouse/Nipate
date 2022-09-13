bfb05b760745f4951f601c9a2f65b61de5b45a89
Dev Banda <wbanda@kabarak.ac.ke>
2022-09-13 14:20:42 +0300

    ☕ Added Select element with styles

--- components/common/selectors/index.tsx
+++ components/common/selectors/index.tsx
@@ -1,12 +1,62 @@
-import {InputHTMLAttributes} from "react";
-import styles from './index.module.scss'
+import { InputHTMLAttributes } from "react";
+import styles from "./index.module.scss";
+import {
+  OptionUnstyled,
+  OptionUnstyledProps,
+  SelectUnstyled,
+  SelectUnstyledProps,
+} from "@mui/base";
 
+/**
+ * Custom styled checkbox input
+ * @param props
+ * @constructor
+ */
 function CheckBox(props: InputHTMLAttributes<HTMLInputElement>) {
-    return <input type="checkbox" className={styles.checkbox} {...props}/>
+  return <input type="checkbox" className={styles.checkbox} {...props} />;
 }
 
+/**
+ * Custom styled radio input
+ * @param props
+ * @constructor
+ */
 function Radio(props: InputHTMLAttributes<HTMLInputElement>) {
-    return <input type="radio" className={styles.radio} {...props}/>
+  return <input type="radio" className={styles.radio} {...props} />;
 }
 
-export {CheckBox, Radio}
+/**
+ * Select Input but with styles
+ * @param className
+ * @param props
+ * @constructor
+ */
+function Select<SValue>(props: SelectUnstyledProps<SValue>) {
+  return (
+    <SelectUnstyled
+      {...props}
+      componentsProps={{
+        root: {
+          className: styles.select_root,
+        },
+        listbox: {
+          className: styles.select_listbox,
+        },
+        popper: {
+          className: styles.select_popper,
+        },
+      }}
+    />
+  );
+}
+
+function Option<OValue>({ className, ...props }: OptionUnstyledProps<OValue>) {
+  return (
+    <OptionUnstyled
+      className={[className, styles.option].join(" ")}
+      {...props}
+    />
+  );
+}
+
+export { CheckBox, Radio, Select, Option };
