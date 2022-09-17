8db4b9f3ceee2ef41b46b34f4bcbfb30a8fc3b6d
Dev Banda <wbanda@kabarak.ac.ke>
2022-09-17 20:11:34 +0300

    ☕ Added Select to component preview

--- pages/components.tsx
+++ pages/components.tsx
@@ -1,47 +1,51 @@
-import { NextPage } from "next";
+import {NextPage} from "next";
 import Head from "next/head";
-import { Button, FormInput, Input, PhoneInput } from "@components/common";
+import {Button, FormInput, Input, Option, Select} from "@components/common";
 
 const Components: NextPage = () => {
-  const planet = new Map<string, string>();
-  const countries: Map<string, string> = new Map([
-    ["france", "🇫🇷"],
-    ["united-kingdom", "🇬🇧"],
-    ["spain", "🇪🇸"],
-  ]);
-  planet.set("name", "earth");
-  planet.set("position", "1");
+    const planet = new Map<string, string>();
+    const countries: Map<string, string> = new Map([
+        ["france", "🇫🇷"],
+        ["united-kingdom", "🇬🇧"],
+        ["spain", "🇪🇸"],
+    ]);
+    planet.set("name", "earth");
+    planet.set("position", "1");
 
-  return (
-    <div className={"flex flex-col items-center justify-center"}>
-      <Head>
-        <title>Components</title>
-      </Head>
-      <main className={"flex flex-col gap-4 py-10"}>
-        <Button>Button</Button>
-        <Button variant={"outline"}>Button</Button>
-        <Button variant={"text"}>Button</Button>
-        <Input />
-        <Input placeholder={"Success Input"} dataValidity={"success"} />
-        <Input placeholder={"Error Input"} dataValidity={"error"} />
-        <div>
-          <span>Label</span> <Input placeholder={"Span Input"} />
+    return (
+        <div className={"flex flex-col items-center justify-center"}>
+            <Head>
+                <title>Components</title>
+            </Head>
+            <main className={"flex flex-col gap-4 py-10"}>
+                <Button>Button</Button>
+                <Button variant={"outline"}>Button</Button>
+                <Button variant={"text"}>Button</Button>
+                <Input/>
+                <Input placeholder={"Success Input"} dataValidity={"success"}/>
+                <Input placeholder={"Error Input"} dataValidity={"error"}/>
+                <div>
+                    <span>Label</span> <Input placeholder={"Span Input"}/>
+                </div>
+                <FormInput inputLabel={"Form Input"}/>
+                <FormInput
+                    inputLabel={"Form Input Success"}
+                    dataValidity={"success"}
+                    feedback={"Success"}
+                />
+                <FormInput
+                    inputLabel={"Form Input Error"}
+                    dataValidity={"error"}
+                    feedback={"Error"}
+                />
+                <Select>
+                    <Option value={0}>H</Option>
+                    <Option value={0}>H</Option>
+                    <Option value={0}>H</Option>
+                </Select>
+            </main>
         </div>
-        <FormInput inputLabel={"Form Input"} />
-        <FormInput
-          inputLabel={"Form Input Success"}
-          dataValidity={"success"}
-          feedback={"Success"}
-        />
-        <FormInput
-          inputLabel={"Form Input Error"}
-          dataValidity={"error"}
-          feedback={"Error"}
-        />
-        <PhoneInput />
-      </main>
-    </div>
-  );
+    );
 };
 
 export default Components;
