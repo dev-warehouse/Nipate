a2dc786216920117b32dd5cb5cdf9e9003815b8d
Dev Banda <wbanda@kabarak.ac.ke>
2022-09-20 15:06:11 +0300

    ☕ remove children wrapper

--- components/layouts/auth/index.tsx
+++ components/layouts/auth/index.tsx
@@ -5,7 +5,7 @@ import styles from './index.module.scss'
 export function AuthLayout(props: HTMLAttributes<HTMLDivElement>) {
     return <div className={styles.auth_root}>
         <Header page={'auth'}/>
-        <main>{props.children}</main>
+        <main className="h-[100%] flex flex-col items-center justify-center">{props.children}</main>
         <Footer/>
     </div>
 }
