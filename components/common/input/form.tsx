c839a326aabcfb2c84f87a7de5f4195f3cac99b1
Dev Banda <wbanda@kabarak.ac.ke>
2022-09-20 14:47:29 +0300

    ☕ Added password visibility

--- components/common/input/form.tsx
+++ components/common/input/form.tsx
@@ -1,9 +1,9 @@
-import {Input, InputProps} from ".";
+import { Input, InputProps } from ".";
 import styles from "./index.module.scss";
-import {Control, useController, UseFormRegister} from "react-hook-form";
-import {Option, Select, SelectProps} from "@components/common";
+import { Control, useController, UseFormRegister } from "react-hook-form";
+import { Option, Select, SelectProps } from "@components/common";
 import Image from "next/image";
-import {ChangeEvent} from "react";
+import { ChangeEvent } from "react";
 
 /**
  * Form Input props, extends Input Props
@@ -11,25 +11,23 @@ import {ChangeEvent} from "react";
  * adds upon inputLabel for form label
  */
 interface FormInputProps extends InputProps {
-    /**
-     * Form Input Label
-     */
-    label: string;
-    /**
-     *Identifier for input in form hook
-     */
-    name: string
-    /**
-     * Attaches component to form hook
-     */
-    register: UseFormRegister<any>
-    /**
-     * Feedback message
-     */
-    errors: any;
-
+  /**
+   * Form Input Label
+   */
+  label: string;
+  /**
+   *Identifier for input in form hook
+   */
+  name: string;
+  /**
+   * Attaches component to form hook
+   */
+  register: UseFormRegister<any>;
+  /**
+   * Feedback message
+   */
+  errors: any;
 }
-
 
 /**
  * Input for Forms with label and validation
@@ -38,19 +36,52 @@ interface FormInputProps extends InputProps {
  * @param props
  * @constructor
  */
-function FormInput({label, name, register, errors, children, dataValidity, ...props}: FormInputProps) {
-    return <div className={styles.form_root}>
-        <label htmlFor={name} className={styles.form_label}
-               data-validity={dataValidity ? dataValidity : errors[name] ? 'error' : 'initial'}>
-            {label}
-        </label>
-        {children ?? (<Input id={name} className={styles.form_input} {...register(name)}
-                             dataValidity={dataValidity ? dataValidity : errors[name] ? 'error' : 'initial'} {...props} />)}
-        {errors[name] ? (
-            <p className={styles.form_message}
-               data-validity={dataValidity ? dataValidity : errors[name] ? 'error' : 'initial'}>{errors[name].message}</p>
-        ) : (<></>)}
-    </div>;
+function FormInput({
+  label,
+  className = "",
+  name,
+  register,
+  errors,
+  children,
+  dataValidity,
+  ...props
+}: FormInputProps) {
+  return (
+    <div className={[className, styles.form_root].join(" ")}>
+      <label
+        htmlFor={name}
+        className={styles.form_label}
+        data-validity={
+          dataValidity ? dataValidity : errors[name] ? "error" : "initial"
+        }
+      >
+        {label}
+      </label>
+      {children ?? (
+        <Input
+          id={name}
+          className={styles.form_input}
+          {...register(name)}
+          dataValidity={
+            dataValidity ? dataValidity : errors[name] ? "error" : "initial"
+          }
+          {...props}
+        />
+      )}
+      {errors[name] ? (
+        <p
+          className={styles.form_message}
+          data-validity={
+            dataValidity ? dataValidity : errors[name] ? "error" : "initial"
+          }
+        >
+          {errors[name].message}
+        </p>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
 }
 
 /**
@@ -59,99 +90,143 @@ function FormInput({label, name, register, errors, children, dataValidity, ...pr
  * @constructor
  */
 function SelectCountries(props: SelectProps) {
-    // Specify for now but it will be fetched
-    const countries: { flag: string, name: string, callingCodes: string }[] = [
-        {
-            flag: "https://flagcdn.com/ke.svg",
-            name: "Kenya",
-            callingCodes: "+254"
+  // Specify for now but it will be fetched
+  const countries: { flag: string; name: string; callingCodes: string }[] = [
+    {
+      flag: "https://flagcdn.com/ke.svg",
+      name: "Kenya",
+      callingCodes: "+254",
+    },
+    {
+      flag: "https://flagcdn.com/tz.svg",
+      name: "Tanzania",
+      callingCodes: "+255",
+    },
+    {
+      flag: "https://flagcdn.com/ug.svg",
+      name: "Uganda",
+      callingCodes: "+256",
+    },
+  ];
 
-        },
-        {
-            flag: "https://flagcdn.com/tz.svg",
-            name: "Tanzania",
-            callingCodes: "+255"
-
-        },
-        {
-            flag: "https://flagcdn.com/ug.svg",
-            name: "Uganda",
-            callingCodes: "+256"
-
-        }
-    ]
-
-    /**
-     * Renders the Select Value
-     * @param option
-     */
-    const renderValue = (option: any) => {
-        if (option !== null) {
-            return <div className={styles.phone_option}>
-                <div className={styles.phone_option_img}>
-                    <Image src={option.label} layout='fill'/>
-                </div>
-                <span>{option.value}</span>
-            </div>
-        }
-        return <div className={styles.phone_option} data-validity={props.dataValidity}>
-            <div className={styles.phone_option_img}/>
-            <p>+123</p>
+  /**
+   * Renders the Select Value
+   * @param option
+   */
+  const renderValue = (option: any) => {
+    if (option !== null) {
+      return (
+        <div className={styles.phone_option}>
+          <div className={styles.phone_option_img}>
+            <Image src={option.label} layout="fill" />
+          </div>
+          <span>{option.value}</span>
         </div>
+      );
     }
+    return (
+      <div className={styles.phone_option} data-validity={props.dataValidity}>
+        <div className={styles.phone_option_img} />
+        <p>+123</p>
+      </div>
+    );
+  };
 
-    return <Select renderValue={renderValue} {...props}>
-        {countries.map((country) => {
-            return <Option key={country.callingCodes} value={country.callingCodes} label={country.flag}>
-                <div className={styles.phone_option}>
-                    <div className={styles.phone_option_img}>
-                        <Image src={country.flag} layout='fill'/>
-                    </div>
-                    <span>({country.callingCodes})</span>
-                    <p>{country.name}</p>
-                </div>
-            </Option>
-        })}
+  return (
+    <Select renderValue={renderValue} {...props}>
+      {countries.map((country) => {
+        return (
+          <Option
+            key={country.callingCodes}
+            value={country.callingCodes}
+            label={country.flag}
+          >
+            <div className={styles.phone_option}>
+              <div className={styles.phone_option_img}>
+                <Image src={country.flag} layout="fill" />
+              </div>
+              <span>({country.callingCodes})</span>
+              <p>{country.name}</p>
+            </div>
+          </Option>
+        );
+      })}
     </Select>
-
+  );
 }
 
 export type PhoneNumber = {
-    code?: string
-    number?: string
+  code?: string;
+  number?: string;
+};
+
+interface PhoneInputProps extends Omit<FormInputProps, "register"> {
+  control: Control<PhoneNumber | any>;
 }
 
-interface PhoneInputProps extends Omit<FormInputProps, 'register'> {
-    control: Control<PhoneNumber | any>
-}
+function PhoneInput({
+  label,
+  name,
+  control,
+  errors,
+  dataValidity,
+  placeholder,
+}: PhoneInputProps) {
+  const {
+    field: { value, onChange },
+  } = useController({
+    control,
+    name,
+    defaultValue: { code: null, number: undefined },
+  });
 
-function PhoneInput({label, name, control, errors, dataValidity, placeholder}: PhoneInputProps) {
+  const handleCodeChange = (data: string) => {
+    onChange({ code: data, number: value.number });
+  };
+  const handleNumberChange = (event: ChangeEvent<HTMLInputElement>) => {
+    onChange({ code: value.code, number: event.target.value });
+  };
 
-    const {field: {value, onChange}} = useController({control, name, defaultValue: {code: null, number: undefined}})
-
-    const handleCodeChange = (data: string) => {
-        onChange({code: data, number: value.number})
-    }
-    const handleNumberChange = (event: ChangeEvent<HTMLInputElement>) => {
-        onChange({code: value.code, number: event.target.value})
-    }
-
-    return <div className={styles.form_root}>
-        <label htmlFor={name} className={styles.form_label}
-               data-validity={dataValidity ? dataValidity : errors[name] ? 'error' : 'initial'}>
-            {label}
-        </label>
-        <div className={styles.phone_root}>
-            <SelectCountries onChange={handleCodeChange}
-                             dataValidity={dataValidity ? dataValidity : errors[name] ? 'error' : 'initial'}/>
-            <Input placeholder={placeholder} onChange={handleNumberChange}
-                   dataValidity={dataValidity ? dataValidity : errors[name] ? 'error' : 'initial'}/>
-        </div>
-        {errors[name] ? (
-            <p className={styles.form_message}
-               data-validity={dataValidity ? dataValidity : errors[name] ? 'error' : 'initial'}>{errors[name].message}</p>
-        ) : (<></>)}
+  return (
+    <div className={styles.form_root}>
+      <label
+        htmlFor={name}
+        className={styles.form_label}
+        data-validity={
+          dataValidity ? dataValidity : errors[name] ? "error" : "initial"
+        }
+      >
+        {label}
+      </label>
+      <div className={styles.phone_root}>
+        <SelectCountries
+          onChange={handleCodeChange}
+          dataValidity={
+            dataValidity ? dataValidity : errors[name] ? "error" : "initial"
+          }
+        />
+        <Input
+          placeholder={placeholder}
+          onChange={handleNumberChange}
+          dataValidity={
+            dataValidity ? dataValidity : errors[name] ? "error" : "initial"
+          }
+        />
+      </div>
+      {errors[name] ? (
+        <p
+          className={styles.form_message}
+          data-validity={
+            dataValidity ? dataValidity : errors[name] ? "error" : "initial"
+          }
+        >
+          {errors[name].message}
+        </p>
+      ) : (
+        <></>
+      )}
     </div>
+  );
 }
 
-export {FormInput, PhoneInput};
+export { FormInput, PhoneInput };
--- components/common/input/index.tsx
+++ components/common/input/index.tsx
@@ -10,13 +10,12 @@ import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
 /**
  * Extends `InputHtmlAttributes` and adds upon dataValidity for data feedback
  */
-interface InputProps {
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   /**
    * This is used to show the various feedback states of the data
    */
   dataValidity?: "initial" | "success" | "error";
 }
-
 
 /**
  * Custom Input that extends HtmlInput with custom styles and user feedback
@@ -25,16 +24,47 @@ interface InputProps {
  * @param props
  * @constructor
  */
-const Input = forwardRef(({
-                              dataValidity,
-                              className,
-                              ...props
-                          }: InputProps & InputUnstyledProps, ref: ForwardedRef<any>) => {
+const Input = forwardRef(
+  (
+    {
+      dataValidity,
+      className,
+      componentsProps,
+      ...props
+    }: InputProps & InputUnstyledProps,
+    ref: ForwardedRef<any>
+  ) => {
+    const [visible, setVisible] = useState<boolean>(false);
+    const End = (): JSX.Element | null => {
+      if (props.type === "password") {
+        return (
+          <div className={styles.icon} onClick={() => setVisible(!visible)}>
+            {visible ? <AiFillEyeInvisible /> : <AiFillEye />}
+          </div>
+        );
+      }
+      return null;
+    };
     inputUnstyledClasses.focused = styles.root_focused;
-    return <InputUnstyled data-validity={dataValidity} className={[className, styles.root].join(' ')}
-                          ref={ref} {...props}/>
-});
+    return (
+      <InputUnstyled
+        endAdornment={<End />}
+        data-validity={dataValidity}
+        componentsProps={{
+          root: componentsProps?.root,
+          input: {
+            ...componentsProps?.input,
+            type: props.type === "password" ? (visible ? "text" : "password") : props.type,
+          },
+        }}
+        className={[className, styles.root].join(" ")}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
 
-export {Input}
-export * from './form'
-export type {InputProps}
+export { Input };
+export * from "./form";
+export type { InputProps };
