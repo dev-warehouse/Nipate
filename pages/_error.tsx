633e5985cef6b4c5b9b89336bc2ae25a36930b2d
Dev Banda <wbanda@kabarak.ac.ke>
2022-09-21 10:52:58 +0300

    ☕ Added custom error page

---
+++ pages/_error.tsx
@@ -0,0 +1,24 @@
+import {NextPageWithLayout} from "@pages/_app";
+import {GlobalLayout} from "@components/layouts/page/layout";
+
+
+const ErrorPage: NextPageWithLayout = (props: any): JSX.Element => {
+    return <div className="h-full flex flex-col justify-center">
+        <p>
+            {props.statusCode
+                ? `An error ${props.statusCode} occurred on server`
+                : 'An error occurred on client'}
+        </p>
+    </div>
+};
+
+ErrorPage.getLayout = (page) => {
+    return <GlobalLayout>{page}</GlobalLayout>;
+};
+
+ErrorPage.getInitialProps = ({res, err}) => {
+    const statusCode = res ? res.statusCode : err ? err.statusCode : 404
+    return {statusCode}
+}
+
+export default ErrorPage
