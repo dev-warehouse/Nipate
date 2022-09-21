eda2a407d3080d64cd81b2a76ec27922f5c6cba5
Dev Banda <wbanda@kabarak.ac.ke>
2022-09-21 17:12:26 +0300

    ☕ Added Auth & Base Urls

---
+++ core/api/urls/auth/index.ts
@@ -0,0 +1,9 @@
+// Login urls
+import {BASE_API_URL} from ".."
+
+export const LOGIN_URL = `${BASE_API_URL}/auth/login`
+export const LOGOUT_URL = `${BASE_API_URL}/auth/logout`
+
+// Logout urls
+export const REGISTER_URL = `${BASE_API_URL}/auth/register`
+export const FINALIZE_REGISTER_URL = `${BASE_API_URL}/auth/register`
