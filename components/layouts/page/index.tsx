195e43f856c7fad43500b6e3fbf9ad2cc6fec97c
Dev Banda <wbanda@kabarak.ac.ke>
2022-09-21 12:56:47 +0300

    ☕ Inline styles

--- components/layouts/page/index.tsx
+++ components/layouts/page/index.tsx
@@ -1,5 +1,4 @@
 import {Footer, Header, HeaderProps} from "@components/pages";
-import styles from '../auth/index.module.scss'
 import Head from "next/head";
 
 export interface GlobalLayoutProps extends HeaderProps {
@@ -11,7 +10,7 @@ export function GlobalLayout({page, title, children}: GlobalLayoutProps) {
         <Head>
             <title>{title}</title>
         </Head>
-        <main className={styles.auth_root}>
+        <main className="h-full flex flex-col items-center">
             <Header page={page}/>
             {children}
             <Footer/>
