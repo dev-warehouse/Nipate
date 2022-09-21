6928013d87f194ebe4e38ea53b036c5a949bf730
Dev Banda <wbanda@kabarak.ac.ke>
2022-09-21 06:45:43 +0300

    ☕ Moved main styles to styles

--- components/layouts/auth/index.tsx
+++ components/layouts/auth/index.tsx
@@ -5,7 +5,7 @@ import styles from './index.module.scss'
 export function AuthLayout(props: HTMLAttributes<HTMLDivElement>) {
     return <div className={styles.auth_root}>
         <Header page={'auth'}/>
-        <main className="h-[100%] flex flex-col items-center justify-center">{props.children}</main>
+        <main className={styles.auth_body}>{props.children}</main>
         <Footer/>
     </div>
 }
