3621f2872ee5c8432993333a11334094bba3ebf1
Dev Banda <wbanda@kabarak.ac.ke>
2022-09-22 23:05:29 +0300

    ☕ Added query client for react queries

--- core/providers/global.tsx
+++ core/providers/global.tsx
@@ -1,11 +1,17 @@
 import {DOMAttributes} from "react";
 import {AuthProvider} from "./auth";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
+const queryClient = new QueryClient()
+
 
 function GlobalProviders(props: DOMAttributes<any>) {
     return <>
-        <AuthProvider>
-            {props.children}
-        </AuthProvider>
+        <QueryClientProvider client={queryClient}>
+            <AuthProvider>
+                {props.children}
+            </AuthProvider>
+        </QueryClientProvider>
     </>
 }
 
