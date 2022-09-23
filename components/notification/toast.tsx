a92f8f5ee3fda77d576bfa238e632fbbf9e47a61
Dev Banda <wbanda@kabarak.ac.ke>
2022-09-23 23:16:44 +0300

    ☕ Added Toast markup and initial styles

--- components/notification/toast.tsx
+++ components/notification/toast.tsx
@@ -1,5 +1,8 @@
 import {ReactNode} from "react";
 import {Status} from "@core/context";
+import styles from "./styles/toast.module.scss";
+import {IoMdClose} from "react-icons/io";
+import {RiBellLine} from "react-icons/ri";
 
 export interface ToastProps {
     message: string;
@@ -17,6 +20,30 @@ export interface ToastProps {
         | "bottom-left";
 }
 
-export function Toast(props: ToastProps) {
-    return <></>
+export function Toast({
+                          message,
+                          status = "notification",
+                          icon,
+                          dismissible = true,
+                          position = "bottom",
+                      }: ToastProps) {
+    return (
+        <div
+            className={[styles.root].join()}
+            data-position={position}
+            data-status={status}
+        >
+            <div className={styles.icon} data-status={status}>
+                <main>{icon ? icon : <RiBellLine/>}</main>
+            </div>
+            <span>{message}</span>
+            {dismissible ? (
+                <div className={styles.close}>
+                    <IoMdClose/>
+                </div>
+            ) : (
+                <></>
+            )}
+        </div>
+    );
 }
