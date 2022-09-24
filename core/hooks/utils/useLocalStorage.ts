a73d4676253087e578c34aff91f2f8ddb82577f3
Dev Banda <wbanda@kabarak.ac.ke>
2022-09-24 21:09:13 +0300

    ☕ Changed useLocalstorage signature

--- core/hooks/utils/useLocalStorage.ts
+++ core/hooks/utils/useLocalStorage.ts
@@ -2,62 +2,64 @@
 import {useCallback, useState} from "react";
 import {useNotification} from "@core/hooks";
 
-const useLocalStorage = (key: string, initialValue: any) => useCallback(() => {
+export function useLocalStorage(key: string, initialValue: any) {
+    return useCallback(() => {
 
-    const {alert} = useNotification()
+        const {alert} = useNotification()
 
-    const readValue = useCallback(() => {
-        if (typeof window === "undefined") {
-            return initialValue;
-        }
-        try {
-            // Get from local storage by key
-            const item = window.localStorage.getItem(key);
-            // Parse stored json or if none return initialValue
-            return item ? JSON.parse(item) : initialValue;
-        } catch (error) {
-            // If error also return initialValue
-            alert([{
-                id: 'localStorage_get_error',
-                type: 'toast',
-                props: {
-                    message: 'Passing from Localstorage failed',
-                    status: 'error'
-                }
-            }])
-            console.log(error);
-            return initialValue;
-        }
-    }, [key, initialValue])
-
-    // State to store our value
-    // Pass initial state function to useState so logic is only executed once
-    const [storedValue, setStoredValue] = useState(readValue);
-    // Return a wrapped version of useState's setter function that ...
-    // ... persists the new value to localStorage.
-    const setValue = (value: any) => {
-        try {
-            // Allow value to be a function so we have same API as useState
-            const valueToStore =
-                value instanceof Function ? value(storedValue) : value;
-            // Save state
-            setStoredValue(valueToStore);
-            // Save to local storage
-            if (typeof window !== "undefined") {
-                window.localStorage.setItem(key, JSON.stringify(valueToStore));
+        const readValue = useCallback(() => {
+            if (typeof window === "undefined") {
+                return initialValue;
             }
-        } catch (error) {
-            // A more advanced implementation would handle the error case
-            alert([{
-                id: 'localStorage_post_error',
-                type: 'toast',
-                props: {
-                    message: 'Setting value to Localstorage failed',
-                    status: 'error'
+            try {
+                // Get from local storage by key
+                const item = window.localStorage.getItem(key);
+                // Parse stored json or if none return initialValue
+                return item ? JSON.parse(item) : initialValue;
+            } catch (error) {
+                // If error also return initialValue
+                alert([{
+                    id: 'localStorage_get_error',
+                    type: 'toast',
+                    props: {
+                        message: 'Passing from Localstorage failed',
+                        status: 'error'
+                    }
+                }])
+                console.log(error);
+                return initialValue;
+            }
+        }, [key, initialValue])
+
+        // State to store our value
+        // Pass initial state function to useState so logic is only executed once
+        const [storedValue, setStoredValue] = useState(readValue);
+        // Return a wrapped version of useState's setter function that ...
+        // ... persists the new value to localStorage.
+        const setValue = (value: any) => {
+            try {
+                // Allow value to be a function so we have same API as useState
+                const valueToStore =
+                    value instanceof Function ? value(storedValue) : value;
+                // Save state
+                setStoredValue(valueToStore);
+                // Save to local storage
+                if (typeof window !== "undefined") {
+                    window.localStorage.setItem(key, JSON.stringify(valueToStore));
                 }
-            }])
-            console.log(error);
-        }
-    };
-    return [storedValue, setValue];
-}, [key, initialValue])
+            } catch (error) {
+                // A more advanced implementation would handle the error case
+                alert([{
+                    id: 'localStorage_post_error',
+                    type: 'toast',
+                    props: {
+                        message: 'Setting value to Localstorage failed',
+                        status: 'error'
+                    }
+                }])
+                console.log(error);
+            }
+        };
+        return [storedValue, setValue];
+    }, [key, initialValue])();
+}
