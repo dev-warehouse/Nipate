5f503c0a3a16d13a00cfd23fc797e6cdb0890f33
Dev Banda <wbanda@kabarak.ac.ke>
2022-09-24 21:27:21 +0300

    ☕ Added success validation and actions

--- core/data/auth/useLogin.ts
+++ core/data/auth/useLogin.ts
@@ -3,19 +3,53 @@ import axios, {AxiosError, AxiosResponse} from "axios";
 import {AuthSerializer, LOGIN_URL, LoginResponseData} from "@core/api";
 import {LoginFormData} from "@core/models";
 import {UseFormReturn} from "react-hook-form";
+import {useAuth, useNotification} from "@core/hooks";
+import {useRouter} from "next/router";
 
 interface UseLoginProps extends Pick<UseFormReturn<LoginFormData>, 'clearErrors' | 'reset' | 'setError'> {
 }
 
 export function useLogin({clearErrors, reset, setError}: UseLoginProps) {
 
+    const {setToken, saveToken} = useAuth()
+    const {alert} = useNotification()
+    const router = useRouter()
+
+    let saveTokenS = false
+
     return useMutation<AxiosResponse<LoginResponseData>, AxiosError<any>, LoginFormData>({
-        mutationFn: (data) => axios.post(LOGIN_URL, AuthSerializer.login(data)),
-        onSuccess: (data) => {
-            console.log(data)
+        mutationFn: ({remember, ...data}) => {
+            saveTokenS = remember;
+            return axios.post(LOGIN_URL, AuthSerializer.login(data))
         },
-        onError: (error) => {
-            console.log(error)
+        onSuccess: ({data: {Auth_token, FirstName}}) => {
+            setToken(Auth_token)
+            if (saveTokenS) saveToken(Auth_token);
+            alert([{
+                id: `welcome_${FirstName}`,
+                type: 'toast',
+                props: {
+                    message: `Welcome back ${FirstName} `,
+                    status: 'success',
+                }
+            }])
+            reset(undefined)
+            clearErrors()
+            router.push('/', 'Welcome')
+        },
+        onError: ({code, response}) => {
+            if (code === AxiosError.ERR_NETWORK) {
+                alert([{
+                    id: 'network_error',
+                    type: 'toast',
+                    props: {
+                        message: 'Unable to Login in',
+                        status: 'error',
+                        icon: ''
+                    }
+                }])
+            }
+            console.log(response)
         },
     })
 }
