ed9983a30e85179ad5de22c5d0c353e3db11e0fa
Dev Banda <wbanda@kabarak.ac.ke>
2022-09-26 08:34:07 +0300

    ☕ User details , locations urls

--- core/api/urls/auth.ts
+++ core/api/urls/auth.ts
@@ -7,3 +7,6 @@ export const LOGOUT_URL = `${BASE_API_URL}/auth/logout`
 // Logout urls
 export const REGISTER_URL = `${BASE_API_URL}/auth/register`
 export const FINALIZE_REGISTER_URL = `${BASE_API_URL}/auth/register`
+
+// User details url
+export const USER_DETAILS_URL = `${BASE_API_URL}/auth/user-details`
