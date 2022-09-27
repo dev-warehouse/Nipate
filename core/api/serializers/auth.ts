025ec37785401d992e6ee43a7a651e799c798dc9
Dev Banda <wbanda@kabarak.ac.ke>
2022-09-28 00:47:21 +0300

    ☕ Added Register serialize rs and response models

--- core/api/serializers/auth.ts
+++ core/api/serializers/auth.ts
@@ -1,4 +1,4 @@
-import {LoginFormData} from "@core/models";
+import {CreateUserFormData, LoginFormData, RegisterUserFormData} from "@core/models";
 
 export interface LoginRequestData {
     MobileNumber: string
@@ -11,10 +11,61 @@ export interface LoginResponseData {
     Auth_token: string
 }
 
+export interface CreateUserPostData {
+    MobileNumber: string
+    IDNumber: number
+    FirstName: string
+    SurName: string
+}
+
+export interface CreateUserResponseData {
+    id: number
+    MobileNumber: string,
+    IDNumber: string,
+    FirstName: string
+}
+
+export interface RegisterUserPostData {
+    UserID: number
+    LocationID: number
+    GenderID: number
+    password: string
+}
+
+
 export class AuthSerializer {
     static login = ({mobile: {code, number}, password}: Omit<LoginFormData, 'remember'>): LoginRequestData => {
         return {
             MobileNumber: `${code}${number}`,
+            password: password
+        }
+    }
+    static createUser = ({
+                             mobile: {code, number},
+                             idNumber,
+                             firstName,
+                             lastName
+                         }: CreateUserFormData): CreateUserPostData => {
+        return {
+            MobileNumber: `${code}${number}`,
+            IDNumber: idNumber,
+            FirstName: firstName,
+            SurName: lastName
+        }
+    }
+
+    static registerUser = ({
+                               createdID,
+                               payload: {
+                                   location,
+                                   gender,
+                                   password
+                               }
+                           }: { createdID: number, payload: RegisterUserFormData }): RegisterUserPostData => {
+        return {
+            UserID: createdID,
+            LocationID: location.id,
+            GenderID: gender === 'male' ? 1 : 2,
             password: password
         }
     }
