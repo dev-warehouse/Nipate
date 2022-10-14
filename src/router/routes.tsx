91115dace1065b288ce4b756842477913a4c166a
Dev Banda <wbanda@kabarak.ac.ke>
2022-10-14 17:13:23 +0300

    👨🏿‍💻 Added Root Layout to Router

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,10 +1,12 @@
 import { RouteObject } from 'react-router-dom'
+import PageRoot from '@pages/app'
 
 const routes: RouteObject[] = []
 
 export default [
   {
     path: '/',
+    element: <PageRoot />,
     children: routes
   }
 ]
