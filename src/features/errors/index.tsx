43a2009b7ee023b08b48abb4df117a28440a189a
Dev Banda <wbanda@kabarak.ac.ke>
2022-10-14 18:48:15 +0300

    👨🏿‍💻 Root Error Boundary

--- src/features/errors/index.tsx
+++ src/features/errors/index.tsx
@@ -1,4 +1,4 @@
-import { ErrorInfo, ReactNode } from 'react'
+import { Component, ErrorInfo, HTMLAttributes, ReactNode } from 'react'
 
 export interface ErrorState {
   hasError: boolean
@@ -8,4 +8,39 @@ export interface ErrorState {
 export interface ErrorProps {
   children: ReactNode
   message: ReactNode
+}
+
+export class RootErrorBoundary extends Component<
+  HTMLAttributes<HTMLDivElement>,
+  ErrorState
+> {
+  componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
+    this.setState({ error: { error, info: errorInfo } })
+  }
+
+  render() {
+    const { state, props } = this
+    if (state.hasError) {
+      return (
+        <div className='w-screen h-screen space-y-2 p-1'>
+          <header className='px-2 py-4 flex flex-row items-center justify-between sticky top-0 backdrop-blur-md'>
+            <a href='/'>
+              <img src='/assets/logo_full.svg' alt='Logo' className='h-8' />
+            </a>
+          </header>
+          <main>
+            <section>TODO Write Message</section>
+            <div>
+              <details style={{ whiteSpace: 'pre-wrap' }}>
+                {state.error.error.message}
+                <br />
+                {state.error.info.componentStack}
+              </details>
+            </div>
+          </main>
+        </div>
+      )
+    }
+    return props.children
+  }
 }
