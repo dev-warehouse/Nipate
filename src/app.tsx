8c27bef0c298074b1ea795e6c69c2f75d693f098
Dev Banda <wbanda@kabarak.ac.ke>
2022-10-14 16:20:16 +0300

    💻 Router setup

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,7 @@
-import React from 'react'
+import Router from '@router/router'
 
 function App() {
-  return (
-    <div className='w-full mb-44 flex flex-col items-center justify-center text-page-hero-sm md:text-page-hero text-center'>
-      Find the best people
-      <br />
-      to serve you
-    </div>
-  )
+  return <Router />
 }
 
 export default App
