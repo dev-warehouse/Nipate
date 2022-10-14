75ccd360058a6274eeebec93a9e3ed56fbd26ddb
Dev Banda <wbanda@kabarak.ac.ke>
2022-10-14 14:04:30 +0300

    💻 Added tailwind styling

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
 function App() {
-  return <div>Nipate Initial</div>
+  return (
+    <div className='w-full mb-44 flex flex-col items-center justify-center text-page-hero-sm md:text-page-hero text-center'>
+      Find the best people
+      <br />
+      to serve you
+    </div>
+  )
 }
 
 export default App
