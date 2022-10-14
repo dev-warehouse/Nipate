d239b2cbfad8fa26145ae30c17c2d2254ec6e929
Dev Banda <wbanda@kabarak.ac.ke>
2022-10-14 14:17:32 +0300

    💻 Added linting support

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { ViteAliases } from "vite-aliases";
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { ViteAliases } from 'vite-aliases'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [ViteAliases({ useConfig: true, useTypescript: true }), react()],
-});
+  // Have the server at port 3000 default
+  server: { host: '0.0.0.0', port: 3000 },
+  plugins: [
+    ViteAliases({
+      useConfig: true,
+      deep: false,
+      depth: 0,
+      useTypescript: true
+    }),
+    react()
+  ]
+})
