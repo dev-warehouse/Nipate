beb18e616af45fddb7435b5c80da408c0d009d85
Dev Banda <wbanda@kabarak.ac.ke>
2022-10-15 17:37:57 +0300

    👨🏿‍💻 Added Page footer

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -1,5 +1,6 @@
 import { Link, Outlet } from 'react-router-dom'
 import RouteErrorHandling from '@router/error'
+import { HTMLAttributes } from 'react'
 import styles from './index.module.scss'
 
 function Header() {
@@ -9,6 +10,18 @@ function Header() {
         <img src='/assets/logo_full.svg' alt='Logo' className='h-8' />
       </Link>
     </header>
+  )
+}
+
+export function PageFooter({ className = '' }: HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div className={[className, styles.footer].join(' ')}>
+      <p>Copyright ©️ 2022 Bespoke Systems</p>
+      <div />
+      <p>Trademark Policy</p>
+      <div />
+      <p>Terms of Service</p>
+    </div>
   )
 }
 
