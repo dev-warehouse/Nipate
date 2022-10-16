b44ca15fc9eeb4041803068b5e819125043f4dff
Dev Banda <wbanda@kabarak.ac.ke>
2022-10-16 07:40:53 +0300

    👨🏿‍💻 Added hero component layout

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,3 +1,33 @@
+import { MdOutlineMyLocation } from 'react-icons/md'
+import { Button } from '@components/buttons'
+import { Input } from '@components/input'
+import { PageFooter } from '@pages/app'
+import styles from './index.module.scss'
+
+function Hero() {
+  return (
+    <div className={styles.hero_root}>
+      <p className={styles.hero_text}>
+        Find the best people
+        <br />
+        to serve you
+      </p>
+      <Input
+        placeholder='Search for Service'
+        className={styles.hero_input}
+        endAdornment={
+          <div className={styles.hero_search_actions}>
+            <div className={styles.hero_location_pin}>
+              <MdOutlineMyLocation />
+            </div>
+            <Button className={styles.hero_search_btn}>Search</Button>
+          </div>
+        }
+      />
+    </div>
+  )
+}
+
 export default function Default() {
   return (
     <div className={styles.page_root}>
