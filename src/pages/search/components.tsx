3d7f0f73173aaeb0de0edd28245b4a4b9b37d2a8
Dev Banda <wbanda@kabarak.ac.ke>
2022-10-17 13:01:51 +0300

    👨🏿‍💻 Added filter item styles

--- src/pages/search/components.tsx
+++ src/pages/search/components.tsx
@@ -1,11 +1,13 @@
 import { DOMAttributes } from 'react'
+import styles from './index.module.scss'
+
 export function FilterItem({
   label,
   children
 }: { label: string } & DOMAttributes<HTMLDivElement>) {
   return (
-    <div className='flex flex-col gap-4'>
-      <p className='text-base font-medium'>{label}</p>
+    <div className={styles.filter_item_container}>
+      <p className={styles.filter_item_label}>{label}</p>
       {children}
     </div>
   )
