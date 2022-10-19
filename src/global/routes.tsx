acd2ca6188823ca15ba137f0ee947c8ff54699ba
Dev Banda <wbanda@kabarak.ac.ke>
2022-10-19 22:45:59 +0300

    ☕ Added Global layouts
    Support for modals is coming smoothly

---
+++ src/global/routes.tsx
@@ -0,0 +1,18 @@
+import { lazy } from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+const LandingPage = lazy(() => import('@pages/landing'))
+const SearchPage = lazy(() => import('@pages/search'))
+const AdvertPage = lazy(() => import('@pages/advert'))
+
+export default function GlobalRoutes() {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+        <Route path='search' element={<SearchPage />} />
+        <Route path='advert' element={<AdvertPage />} />
+      </Routes>
+    </div>
+  )
+}
