752c2c056d3614fd585c4264b6c3e8df5505e1ac
Dev Banda <wbanda@kabarak.ac.ke>
2022-10-20 02:11:45 +0300

    ☕ Renamed to components

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,7 +1,7 @@
 import { MdOutlineMyLocation } from 'react-icons/md'
 import { Button } from '@components/ui/buttons'
 import { Input } from '@components/ui/input'
-import Services from '@pages/landing/services'
+import Services from '@pages/landing/components'
 import { Link } from 'react-router-dom'
 import PageFooter from '@/components/page/footer'
 import styles from './index.module.scss'
