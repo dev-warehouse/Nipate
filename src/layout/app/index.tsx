c31348977205e1973cb77cfab6a6e55f7bfcf6a2
Dev Banda <wbanda@kabarak.ac.ke>
2022-10-19 19:10:46 +0300

    ☕ Refactored page root to layout components

--- src/layout/app/index.tsx
+++ src/layout/app/index.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom'
 import RouteErrorHandling from '@router/error'
-import PageHeader from '@/components/page/header'
+import PageHeader from '@components/page/header'
 import styles from './index.module.scss'
 
 function PageRoot() {
