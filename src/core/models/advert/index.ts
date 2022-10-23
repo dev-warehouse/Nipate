e2883d3a7ff74812713121cf8f92d021090d00b5
Dev Banda <wbanda@kabarak.ac.ke>
2022-10-23 07:05:53 +0300

    ☕ Added more types to advert model

--- src/core/models/advert/index.ts
+++ src/core/models/advert/index.ts
@@ -1,12 +1,13 @@
-import { ServiceCategory } from '../service'
-import { User, UserLocation } from '../user'
+import { County, Town } from '@core/models/location'
+import { Service } from '../service'
+import { User } from '../user'
 
 export interface Advert {
   id: number
   ADTitle: string
   User: User
-  Service: ServiceCategory
-  Location: UserLocation
+  Service: Service
+  Location: County | Town
   AdDescription: string
   StartDate: Date
   ExpiryDate: Date
