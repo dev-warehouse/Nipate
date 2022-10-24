3597d5108d4195e4e5f7165bd4a141603b09213c
Dev Banda <wbanda@kabarak.ac.ke>
2022-10-24 11:01:32 +0300

    ☕ Added location data routes

---
+++ src/api/hooks/location/index.ts
@@ -0,0 +1,30 @@
+import { County, Town } from '@/api/models/location'
+import { useAxios } from '@/core/hooks/axios'
+import { useQuery } from '@tanstack/react-query'
+
+export function useCounties() {
+  const axios = useAxios()
+
+  return useQuery<County[]>(['counties'], async () => {
+    const { data } = await axios.get<County[]>(``)
+    return data
+  })
+}
+
+export function useLocations() {
+  const axios = useAxios()
+
+  return useQuery<Location[]>(['counties'], async () => {
+    const { data } = await axios.get<Location[]>(``)
+    return data
+  })
+}
+
+export function useTowns() {
+  const axios = useAxios()
+
+  return useQuery<Town[]>(['counties'], async () => {
+    const { data } = await axios.get<Town[]>(``)
+    return data
+  })
+}
