7820816c5e752757aedaa3b1ddccfff40cc22831
Dev Banda <wbanda@kabarak.ac.ke>
2022-10-24 11:01:13 +0300

    ☕ Added service data routes

---
+++ src/api/hooks/services/index.ts
@@ -0,0 +1,21 @@
+import { Service, ServiceCategory } from '@/api/models/service'
+import { useAxios } from '@/core/hooks/axios'
+import { useQuery } from '@tanstack/react-query'
+
+export function useService() {
+  const axios = useAxios()
+
+  return useQuery<Service[]>(['service'], async () => {
+    const { data } = await axios.get<Service[]>(``)
+    return data
+  })
+}
+
+export function useServiceCategories() {
+  const axios = useAxios()
+
+  return useQuery<ServiceCategory[]>(['service-categories'], async () => {
+    const { data } = await axios.get<ServiceCategory[]>(``)
+    return data
+  })
+}
