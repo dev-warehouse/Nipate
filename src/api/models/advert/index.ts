9d9d227621a3a8017cc44b90021f1adaa147e8cc
Dev Banda <wbanda@kabarak.ac.ke>
2022-10-24 19:42:04 +0300

    ☕ Updated advert model

--- src/api/models/advert/index.ts
+++ src/api/models/advert/index.ts
@@ -1,15 +1,15 @@
 import { County, Town } from '../location'
+import { Provider } from '../provider'
 import { Service } from '../service'
-import { User } from '../user'
 
 export interface Advert {
   id: number
-  ADTitle: string
-  User: User
-  Service: Service
-  Location: County | Town
-  AdDescription: string
-  StartDate: Date
-  ExpiryDate: Date
-  NoOfMessages: number
+  title: string
+  provider: Provider
+  service: Service
+  location: County | Town
+  description: string
+  startDate: Date
+  expiryDate: Date
+  noOfMessages: number
 }
