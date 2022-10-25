b4c992c4ce2abd48bb287be96b9aa02a7c0eca5b
Dev Banda <wbanda@kabarak.ac.ke>
2022-10-25 12:25:51 +0300

    ☕ User deserializer updated

--- src/api/serializers/user/index.ts
+++ src/api/serializers/user/index.ts
@@ -1,4 +1,5 @@
-import { User } from '@/api/models/user'
+import { Gender, User, UserDetails } from '@/api/models/user'
+import { countyDeserializer, CountyResponse } from '../location'
 
 export interface UserResponse {
   id: number
@@ -6,20 +7,61 @@ export interface UserResponse {
   IDNumber: string
   FirstName: string
   SurName: string
+  Avatar?: string
 }
 
-export function UserDeserializer({
+export function userDeserializer({
   id,
   MobileNumber,
   FirstName,
   SurName,
-  IDNumber
+  IDNumber,
+  Avatar
 }: UserResponse): User {
   return {
     userId: id,
-    avatar: `${id}-${FirstName}`,
+    avatar:
+      Avatar ??
+      `https://avatars.dicebear.com/api/adventurer/${id}-${FirstName}.svg`,
     idNumber: IDNumber,
     displayName: `${FirstName} ${SurName}`,
     mobileNumber: MobileNumber
+  }
+}
+
+export interface UserDetailsResponse {
+  id: number
+  MobileNumber: string
+  IDNumber: string
+  FirstName: string
+  SurName: string
+  Location: CountyResponse
+  Avatar?: string
+  Gender: Gender
+}
+
+export function userDetailsDeserilizer({
+  id,
+  MobileNumber,
+  IDNumber,
+  FirstName,
+  SurName,
+  Location,
+  Avatar,
+  // eslint-disable-next-line @typescript-eslint/no-shadow
+  Gender
+}: UserDetailsResponse): UserDetails {
+  return {
+    userId: id,
+    mobileNumber: MobileNumber,
+    idNumber: IDNumber,
+    firstName: FirstName,
+    displayName: `${FirstName} ${SurName}`,
+    surName: SurName,
+    gender: Gender,
+    location: countyDeserializer(Location),
+    avatar:
+      Avatar ??
+      `https://avatars.dicebear.com/api/adventurer/${id}-${FirstName}.svg`
   }
 }
