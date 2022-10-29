f0b1e89dbdb435f292f9d6266161f588e17fd040
Dev Banda <wbanda@kabarak.ac.ke>
2022-10-30 01:27:05 +0300

    🐛 Fixed route order bug

--- src/auth/routes.tsx
+++ src/auth/routes.tsx
@@ -1,15 +1,17 @@
 import AuthLayout from '@layout/auth'
 import { lazy } from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 
 const Login = lazy(() => import('./pages/login'))
 const Register = lazy(() => import('./pages/register'))
 
 export default function AuthRoutes() {
   return (
-    <Route element={<AuthLayout />}>
-      <Route path='' element={<Login />} />
-      <Route path='register' element={<Register />} />
-    </Route>
+    <Routes>
+      <Route element={<AuthLayout />}>
+        <Route path='' element={<Login />} />
+        <Route path='register' element={<Register />} />
+      </Route>
+    </Routes>
   )
 }
