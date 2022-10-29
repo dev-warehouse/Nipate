a78e56d39bcf28a087f10b2ed048e035d4bfdb88
Dev Banda <wbanda@kabarak.ac.ke>
2022-10-30 01:20:39 +0300

    ☕ Added Login and Register route pages

--- src/auth/routes.tsx
+++ src/auth/routes.tsx
@@ -1,11 +1,15 @@
 import AuthLayout from '@layout/auth'
+import { lazy } from 'react'
 import { Route } from 'react-router-dom'
+
+const Login = lazy(() => import('./pages/login'))
+const Register = lazy(() => import('./pages/register'))
 
 export default function AuthRoutes() {
   return (
     <Route element={<AuthLayout />}>
-      <Route path='/' />
-      <Route path='register' />
+      <Route path='' element={<Login />} />
+      <Route path='register' element={<Register />} />
     </Route>
   )
 }
