e29aa6031263c834866ae82ae5904e0dbcf3df11
Dev Banda <wbanda@kabarak.ac.ke>
2022-11-03 14:27:35 +0300

    ☕ Setup Auth Provider

--- src/auth/context/auth.tsx
+++ src/auth/context/auth.tsx
@@ -1,5 +1,6 @@
-import { createContext, useContext } from 'react'
+import { createContext, ProviderProps, useContext } from 'react'
 import { UserDetails } from '@api/models/user'
+import { useQueryClient } from '@tanstack/react-query'
 
 interface AuthContextProps {
   userData: UserDetails
@@ -12,4 +13,17 @@ const AuthContext = createContext<AuthContextProps>({} as AuthContextProps)
 
 export function useAuth() {
   return useContext(AuthContext)
+}
+
+export default function AuthProvider({
+  children
+}: Omit<ProviderProps<never>, 'value'>) {
+  const queryClient = useQueryClient()
+  const setToken = (token: string) => {}
+  const logout = () => {}
+  return (
+    <AuthContext.Provider value={{ setToken, logout }}>
+      {children}
+    </AuthContext.Provider>
+  )
 }
