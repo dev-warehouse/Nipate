177292bedde96106e68040a067e66a9c0e9a1c7f
Dev Banda <wbanda@kabarak.ac.ke>
2022-11-03 19:56:44 +0300

    ☕ UseCrypto initial

---
+++ src/core/hooks/utils/useCrypto.ts
@@ -0,0 +1,9 @@
+/**
+ * Hook for encrypting and decrypting
+ */
+export default function useCrypto() {
+  const encrypt = ''
+  const decrypt = ''
+  const hash = ''
+  return { encrypt, decrypt, hash }
+}
