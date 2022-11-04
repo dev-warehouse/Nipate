50d26330678e199c71439cee36d96e6601c4c69a
Dev Banda <wbanda@kabarak.ac.ke>
2022-11-04 11:57:52 +0300

    ☕ Added hash function to useCrypto

--- src/core/hooks/utils/useCrypto.ts
+++ src/core/hooks/utils/useCrypto.ts
@@ -1,13 +1,14 @@
-import { enc } from 'crypto-js'
-import AES from 'crypto-js/aes'
+import { AES, enc, MD5 } from 'crypto-js'
 
 /**
  * Hook for encrypting and decrypting
  */
-export default function useCrypto(hash: string) {
+export default function useCrypto(key: string) {
   const encrypt = (message: string): string =>
-    AES.encrypt(message, hash).toString()
+    AES.encrypt(message, key).toString()
   const decrypt = (cipherText: string): string =>
-    AES.decrypt(cipherText, hash).toString(enc.Utf8)
-  return { encrypt, decrypt }
+    AES.decrypt(cipherText, key).toString(enc.Utf8)
+
+  const hash = (text: string) => MD5(text).toString(enc.Utf8)
+  return { encrypt, decrypt, hash }
 }
