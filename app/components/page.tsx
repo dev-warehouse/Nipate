34884574c480bdc6b6f0cac956f28e2e8e67f155
Walter Banda <walterkaimurima@gmail.com>
2022-12-19 13:02:38 +0300

    ☕ Introduced design system page

---
+++ app/components/page.tsx
@@ -0,0 +1,10 @@
+import { Input } from '@design/input'
+
+export default function Components() {
+  return (
+    <div className='flex flex-col gap-4'>
+      <p className='text-lg font-bold text-center'>Components</p>
+      <Input placeholder='Hello sample Input' />
+    </div>
+  )
+}
