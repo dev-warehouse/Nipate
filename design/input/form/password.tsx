8da87600806c3a27d8625bf45535ee9b7fa03bbb
Walter Banda <walterkaimurima@gmail.com>
2022-12-19 16:58:38 +0300

    ☕ Password toggle logic

--- design/input/form/password.tsx
+++ design/input/form/password.tsx
@@ -1,9 +1,31 @@
+'use client'
+
 import FormInput, { ControlledFormElement } from '.'
+import { VscEye, VscEyeClosed } from 'react-icons/vsc'
+import { useState } from 'react'
+
+import styles from './index.module.scss'
+import { ButtonUnstyled } from '@mui/base'
 
 export default function PasswordInput({
   register,
   errors,
   ...props
 }: ControlledFormElement<any>) {
-  return <FormInput {...props} />
+  const [visible, changeVisibility] = useState<boolean>(false)
+
+  return (
+    <FormInput
+      {...props}
+      type={visible ? 'text' : 'password'}
+      endAdornment={
+        <ButtonUnstyled
+          onClick={() => changeVisibility(state => !state)}
+          className={styles.passwordToggle}
+        >
+          {visible ? <VscEye /> : <VscEyeClosed />}
+        </ButtonUnstyled>
+      }
+    />
+  )
 }
