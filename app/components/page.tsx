5dd3c9b5d4a6bfca06463c3f6d34795f48cb18f3
Walter Banda <walterkaimurima@gmail.com>
2022-12-19 14:13:57 +0300

    ☕ Form states introduced

--- app/components/page.tsx
+++ app/components/page.tsx
@@ -5,6 +5,24 @@ export default function Components() {
     <div className='flex flex-col gap-4'>
       <p className='text-lg font-bold text-center'>Components</p>
       <Input placeholder='Hello sample Input' />
+      <Input placeholder='Success' dataValidity="success" />
+      <Input placeholder='Warning' dataValidity="warning" />
+      <Input placeholder='Error' dataValidity="error" />
+      <FormInput
+        label='Form Input'
+      />
+      <FormInput
+        label='Success Form'
+        feedback={{ message: 'success', variant: 'success' }}
+      />
+      <FormInput
+        label='Warning Form'
+        feedback={{ message: 'warning', variant: 'warning' }}
+      />
+      <FormInput
+        label='Error Form'
+        feedback={{ message: 'error', variant: 'error' }}
+      />
     </div>
   )
 }
